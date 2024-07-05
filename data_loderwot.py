8c91b0b49c71016005457eb2723700473735af31
pratyush <82261854+helloiampratyush@users.noreply.github.com>
2024-07-05 10:11:29 +0530

    update ones

---
+++ data_loderwot.py
@@ -0,0 +1,31 @@
+import torchvision
+from torch.utils.data import DataLoader
+from torchvision import datasets,transforms
+import os
+
+num_workers=os.cpu_count()
+# print(num_workers)
+def dataloader_creator(train_dir,test_dir,batch_size:int,
+                       data_transforms):
+    
+    #creating test_data and train data
+
+    train_data=datasets.ImageFolder(root=train_dir,
+                                    transform=data_transforms,
+                                    target_transform=None)
+    
+    test_data=datasets.ImageFolder(root=test_dir,
+                                   transform=data_transforms,
+                                   target_transform=None)
+    #class_names
+
+    class_names=train_data.classes
+    
+    #creating data loader
+    train_dataloader=DataLoader(dataset=train_data,batch_size=batch_size,
+                                shuffle=True,num_workers=num_workers,pin_memory=True)
+    
+    test_dataloader=DataLoader(dataset=test_data,batch_size=batch_size,shuffle=False,
+                               num_workers=num_workers,pin_memory=True)
+    
+    return class_names,train_dataloader,test_dataloader
