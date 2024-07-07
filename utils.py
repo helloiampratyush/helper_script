20cf5e9a662dd3baac945c3af5f09562986b2c6d
pratyush <82261854+helloiampratyush@users.noreply.github.com>
2024-07-07 10:05:44 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 import torch
 from pathlib import Path
 
-def save_model(model:torch.nn.Module):
+def save_model(model:torch.nn.Module,save_file_path:str):
 
- save_dir=Path("model")
+ save_dir=Path("models")
  
- model_save_path=save_dir/"mini_foodvision_101v1"
+ model_save_path=save_dir/save_file_path
 
  # checking if directory is already available
  if save_dir.is_dir():
