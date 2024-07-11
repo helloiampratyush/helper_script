ca6aa09114048cc76020be997dc73fcdfcff0639
pratyush <82261854+helloiampratyush@users.noreply.github.com>
2024-07-11 10:18:52 +0530

    Update plot_all_wrong.py

--- plot_all_wrong.py
+++ plot_all_wrong.py
@@ -44,7 +44,7 @@ def plot_all_wrong_predicted(dis_prediction_tensor:list,
 
             plt.imshow(img.permute(1,2,0))
 
-            plt.title(f"predicted label: {class_names[predicted_label_tensor[i]]} | true label : {class_names[predicted_label_tensor[i]]} | prediction probability : {prediction[i]:.4f}")
+            plt.title(f"predicted label: {class_names[predicted_label_tensor[i]]} | true label : {class_names[true_label_tensor[i]]} | prediction probability : {prediction[i]:.4f}")
 
             plt.axis(False)
 
